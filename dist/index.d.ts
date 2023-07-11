dc8877b8eea168ef0f8542cfccf7ac7ac531bd2d
Lam Hieu <lamhieu.vk@gmail.com>
2023-07-11 17:51:59 +0700

    chore: delete `dist` directory

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-export { type EnhancedPromise, default as Quiz, type QuizCallbackError, type QuizDeferifyPromise, type QuizDeferPromise, type QuizFunctionCallback, type QuizFunctionWithCallback, type QuizPromiseExecuter, type QuizPromiseOptions, type QuizQueueItem, type QuizThroatOptions, } from './Quiz';
-export { default as fastObjectProperties, } from './utilities/fastObjectProperties';
-export { default as throat, } from './utilities/throat';
--- dist/utilities/fastObjectProperties.d.ts
+++
@@ -1,2 +0,0 @@
-declare function fastObjectProperties<T extends object>(data?: T): T;
-export default fastObjectProperties;
